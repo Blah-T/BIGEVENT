a82b7366395b5e82e3759e5b7bd116ec2cb8c198
Blah <50124591@qq.com>
2020-09-13 11:42:23 +0800

    完成了首页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,3 +1,21 @@
 $.ajaxPrefilter(function (options) {
+  //发起Ajax请求之前,同意拼接请求的根路径
   options.url = "http://ajax.frontend.itheima.net" + options.url;
+
+  //统一为有权限的接口,设置headers请求头
+  if (options.url.indexOf("/my/") !== -1) {
+    options.headers = {
+      Authorization: localStorage.getItem("token") || "",
+    };
+  }
+
+  //调用complete回调函数
+  options.complete = function (res) {
+    if (res.responseJSON.status === 1) {
+      //强制清空token
+      localStorage.removeItem("token");
+      //强制跳转到登录页面
+      location.href = "/login.html";
+    }
+  };
 });
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,28 @@
 $(function () {
   getUserInfo();
+  var layer = layui.layer;
+
+  //点击按钮实现退出功能
+  $("#btnLogout").on("click", function () {
+    //提示用户是否退出
+    layer.confirm(
+      "确定退出登录?",
+      {
+        icon: 3,
+        title: "提示",
+      },
+      function (index) {
+        //do something
+        //清空本地存储中的token
+        localStorage.removeItem("token");
+        //跳转到登录页面
+        location.href = "/login.html";
+
+        //关闭comfirm询问框[]
+        layer.close(index);
+      }
+    );
+  });
 });
 
 //获取用户的基本信息
@@ -8,11 +31,43 @@ function getUserInfo() {
     method: "GET",
     url: "/my/userinfo",
     //请求头配置对象
-    headers: {
-      Authorization: localStorage.getItem("token") || "",
-    },
+    // headers: {
+    //   Authorization: localStorage.getItem("token") || "",
+    // },
     success: function (res) {
-      console.log(res);
+      if (res.status !== 0) {
+        return layui.layer.msg("获取用户信息失败!");
+      }
+      //调用renderAvatar渲染用户头像
+      renderAvatar(res.data);
     },
+
+    /* //调用complete回调函数
+    complete: function (res) {
+      if (res.responseJSON.status === 1) {
+        //强制清空token
+        localStorage.removeItem("token");
+        //强制跳转到登录页面
+        location.href = "/login.html";
+      }
+    }, */
   });
+}
+
+function renderAvatar(user) {
+  //获取用户的名称
+  var name = user.nickname || user.username;
+  //设置欢迎的文本
+  $("#wellcome").html("欢迎&nbsp;&nbsp" + name);
+  //渲染用户的头像
+  if (user.user_pic !== null) {
+    //渲染图片头像
+    $(".layui-nav-img").attr("src", user.user_pic).show();
+    $(".text-avatar").hide();
+  } else {
+    //渲染文本头像
+    $(".layui-nav-img").hide();
+    var first = name[0].toUpperCase();
+    $(".text-avatar").html(first).show();
+  }
 }
